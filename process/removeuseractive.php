7b71df5bd4d264a6586cd10bcbf1efb3c586fbf2
Laksamadi Guko <elru.lakusa@gmail.com>
2018-04-02 22:25:58 +0700

    Add files via upload

--- process/removeuseractive.php
+++ process/removeuseractive.php
@@ -19,7 +19,20 @@ session_start();
 // hide all error
 error_reporting(0);
 
+// get user active
+$getuser = $API->comm("/ip/hotspot/active/print", array(
+"?.id"=> "$removeuseractive",));
+$user =	$getuser[0]['user'];
+// get cookie id
+$getcookie = $API->comm("/ip/hotspot/cookie/print", array(
+"?user"=> "$user",));
+$ck =	$getcookie[0]['.id'];
+// remove cookie
+$API->comm("/ip/hotspot/cookie/remove", array(
+".id"=> "$ck",));
+// remove user active
 $API->comm("/ip/hotspot/active/remove", array(
 ".id"=> "$removeuseractive",));
+// redirect to user active
 echo "<script>window.location='./?hotspot=active'</script>";
 ?>
