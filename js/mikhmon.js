4899bcc7da8d39482ef66c9a2a4a2525ef8ae6f9
Laksamadi Guko <elru.lakusa@gmail.com>
2018-05-15 19:46:00 +0700

    Add files via upload

--- js/mikhmon.js
+++ js/mikhmon.js
@@ -160,3 +160,20 @@ function fTable2() {
   }
 }
 
+function fTable3() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("filterTable3");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("tFilter");
+  tr = table.getElementsByTagName("tr");
+  for (i = 1; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[4];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
