5b3f785fc4d55b95e2e213a7598478f46afb692c
Laksamadi Guko <elru.lakusa@gmail.com>
2018-06-01 00:55:26 +0700

    Add files via upload

---
+++ status/status.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ *  Copyright (C) 2018 Laksamadi Guko.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+session_start();
+// hide all error
+error_reporting(0);
+include_once('../include/config.php');
+include_once('../lib/routeros_api.class.php');
+
+$API = new RouterosAPI();
+$API->debug = false;
+$API->connect( $iphost, $userhost, $passwdhost );
+
+$uname = $_GET['name'];
+if($uname != ""){
+  $getname = $API->comm("/sys/sch/print", array("?name" => "$uname"));
+  $exp = $getname[0]['next-run'];
+  
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Voucher-<?php echo $hotspotname."-".$uname;?></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="pragma" content="no-cache" />
+		<style>
+		body {
+  			font-family: 'Helvetica', arial, sans-serif;
+			font-size: 15px;
+			margin:0px;
+  		}
+		</style>
+	</head>
+	<body>
+		<div style="padding:5px;" id="exp" ><?php echo $exp;?></div>
+	</body>
+</html>
